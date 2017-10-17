6cfaa1d1f189b2511af670ac93fa36cb7b348e56
robertoprato <roberto.prato@gmail.com>
2017-10-17 21:45:35 +0100

    Develop (#1)
    * updated gradle config and added ExoPlayer dependencies
    
    * use Exoplayer for Audio content
    
    * added option to drag to position in audio file
    
    * cleaned testfiles
    
    * set version to 5.1
    
    * SimpleExoPlayer instance & added audioAttributes
    
    * added clean player call
    
    * small fixes
    
    * removed toast
    
    * rich version updated
    
    * small fix
    
    * remove

--- richtext/src/main/java/io/square1/richtextlib/ui/audio/AudioPlayer.java
+++ richtext/src/main/java/io/square1/richtextlib/ui/audio/AudioPlayer.java
@@ -19,15 +19,36 @@
 
 package io.square1.richtextlib.ui.audio;
 
-import android.app.Activity;
 import android.content.Context;
-
-import android.media.MediaPlayer;
-import android.os.Bundle;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.text.TextUtils;
 import android.widget.Toast;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.audio.AudioAttributes;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,21 +59,28 @@ import io.square1.richtextlib.R;
 /**
  * Created by roberto on 17/09/15.
  */
-public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
+public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider,
+        PlayerListener.PlayerObserver {
 
-    private static HashMap<String, Media> mMediaMap;
+    public static int DEFAULT_TIME_OUT = 60000;
 
-    private HashMap<AudioPlayerHolder, String> mAudioToPlayer;
+    protected static HashMap<String, Media> mMediaMap;
 
-    private MediaPlayer mMediaPlayer;
-    private Context mContext;
-    private String mCurrentFile;
-    private String mPendingFile;
-    private Handler mHandler;
+    protected HashMap<AudioPlayerHolder, String> mAudioToPlayer;
+
+    protected PlayerListener mPlayerListener;
+    protected ExoPlayer mMediaPlayer;
+    protected Context mContext;
+    protected String mCurrentFile;
+    protected String mPendingFile;
+    protected Handler mHandler;
+
+    protected String mAppName;
 
     public AudioPlayer(Context activity) {
 
         mContext = activity.getApplicationContext();
+        mAppName = getApplicationName();
         mAudioToPlayer = new HashMap<>();
         mMediaMap = new HashMap<>();
         mHandler = new Handler() {
@@ -71,7 +99,7 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
 
     }
 
-    private Media getMedia(String file) {
+    protected Media getMedia(String file) {
 
         Media media = mMediaMap.get(file);
         if (media == null) {
@@ -83,7 +111,7 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
         return media;
     }
 
-    private void synchronizeHolders() {
+    protected void synchronizeHolders() {
 
         Set<AudioPlayerHolder> keys = mAudioToPlayer.keySet();
         for (AudioPlayerHolder holder : keys) {
@@ -91,16 +119,15 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
         }
     }
 
-    private void synchronizeCurrent() {
+    protected void synchronizeCurrent() {
 
         Set<Map.Entry<AudioPlayerHolder, String>> entries = mAudioToPlayer.entrySet();
         for (Map.Entry<AudioPlayerHolder, String> entry : entries) {
             if (TextUtils.equals(entry.getValue(), mCurrentFile)) {
                 entry.getKey().synchronizeState();
-                mHandler.sendEmptyMessageDelayed(1, 1000);
-                return;
             }
         }
+        mHandler.sendEmptyMessageDelayed(1, 1000);
     }
 
     public void onCreate() {
@@ -148,18 +175,33 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
 
             replaceCurrentPLayer();
 
-            //prepare
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
 
-            MediaPlayer mediaPlayer = new MediaPlayer();
+            TrackSelection.Factory audioTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            TrackSelector trackSelector = new DefaultTrackSelector(audioTrackSelectionFactory);
 
-            mediaPlayer.setOnPreparedListener(this);
-            mediaPlayer.setOnCompletionListener(this);
-            mediaPlayer.setOnErrorListener(this);
+            SimpleExoPlayer mediaPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector);
+
+            mediaPlayer.setAudioAttributes(new AudioAttributes.Builder().setContentType(AudioManager.STREAM_MUSIC).build());
+
+            mediaPlayer.setPlayWhenReady(true);
+            mPlayerListener = new PlayerListener(mediaPlayer, this);
+            mediaPlayer.addListener(mPlayerListener);
+
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+            DefaultHttpDataSourceFactory dataSource = new DefaultHttpDataSourceFactory(mAppName,
+                    null, DEFAULT_TIME_OUT, DEFAULT_TIME_OUT, true);
 
             try {
+
                 mPendingFile = audio;
-                mediaPlayer.setDataSource(audio);
-                mediaPlayer.prepareAsync();
+                Handler handler = new Handler(Looper.getMainLooper());
+                MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(audio),
+                        dataSource, extractorsFactory, handler, mPlayerListener);
+                // Prepare the player with the source.
+                mediaPlayer.prepare(mediaSource);
+                mMediaPlayer = mediaPlayer;
             }
             catch (Exception exc) {
                 if (mediaPlayer != null) {
@@ -169,8 +211,8 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
             }
         }
         else if (mMediaPlayer != null &&
-                mMediaPlayer.isPlaying() == false) {
-            mMediaPlayer.start();
+                mMediaPlayer.getPlayWhenReady() == false) {
+            mMediaPlayer.setPlayWhenReady(true);
             mHandler.sendEmptyMessage(1);
             notifyState();
         }
@@ -185,7 +227,7 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
         }
 
         if (TextUtils.equals(mCurrentFile, audio) == true) {
-            mMediaPlayer.pause();
+            mMediaPlayer.setPlayWhenReady(false);
             mHandler.removeMessages(0);
             mHandler.removeMessages(1);
             notifyState();
@@ -212,7 +254,7 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
     @Override
     public int getDuration(String audio) {
 
-        return getMedia(audio).duration;
+        return (int) getMedia(audio).duration;
     }
 
     @Override
@@ -220,7 +262,7 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
 
         if (mMediaPlayer != null &&
                 TextUtils.equals(mCurrentFile, audio)) {
-            return mMediaPlayer.getCurrentPosition();
+            return (int) mMediaPlayer.getCurrentPosition();
         }
 
         return 0;
@@ -231,7 +273,7 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
 
         if (mMediaPlayer != null &&
                 TextUtils.equals(audio, mCurrentFile)) {
-            return mMediaPlayer.isPlaying();
+            return mMediaPlayer.getPlayWhenReady();
         }
         if (TextUtils.equals(mPendingFile, audio) == true) {
             return true;
@@ -280,15 +322,15 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
         }
     }
 
-    private void seek(String file, boolean forward) {
+    protected void seek(String file, boolean forward) {
 
         if (TextUtils.equals(mCurrentFile, file) &&
                 mMediaPlayer != null) {
 
-            int currentPosition = mMediaPlayer.getCurrentPosition();
+            long currentPosition = mMediaPlayer.getCurrentPosition();
             Media media = getMedia(file);
             if (media.duration == Media.DURATION_UNKNOWN) {
-                int duration = mMediaPlayer.getDuration();
+                long duration = mMediaPlayer.getDuration();
                 media.duration = duration;
             }
             final int delta = 2 * 1000;
@@ -301,46 +343,87 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
             mMediaPlayer.seekTo(currentPosition);
 
         }
+
     }
 
-    private void notifyState() {
+    @Override
+    public void seek(String file, long position) {
+
+        if (TextUtils.equals(mCurrentFile, file) &&
+                mMediaPlayer != null) {
+
+            Media media = getMedia(file);
+            if (media.duration == Media.DURATION_UNKNOWN) {
+                long duration = mMediaPlayer.getDuration();
+                media.duration = duration;
+            }
+
+            mMediaPlayer.seekTo(position);
+
+        }
+    }
+
+    protected void notifyState() {
 
         mHandler.sendEmptyMessage(0);
     }
 
     @Override
-    public void onPrepared(MediaPlayer mp) {
+    public void onSeekComplete(ExoPlayer player) {
 
-        mMediaPlayer = mp;
-        //ready to play
-        mCurrentFile = mPendingFile;
-        mPendingFile = null;
-        Media media = getMedia(mCurrentFile);
-        if (media.duration == Media.DURATION_UNKNOWN) {
-            int duration = mMediaPlayer.getDuration();
-            media.duration = duration;
-        }
-        mMediaPlayer.start();
-        notifyState();
-        mHandler.sendEmptyMessageDelayed(1, 1000);
     }
 
     @Override
-    public void onCompletion(MediaPlayer mp) {
+    public void onBufferingUpdate(ExoPlayer player, int progress) {
 
-        if (mp == mMediaPlayer) {
+    }
+
+    @Override
+    public void onCompletion(ExoPlayer player) {
+
+        if (player == mMediaPlayer) {
             //ready to play
             notifyState();
             cleanCurrentPLayer();
         }
+
     }
 
     @Override
-    public boolean onError(MediaPlayer mp, int what, int extra) {
+    public void onPrepared(ExoPlayer player) {
 
-        Toast.makeText(mContext,
-                R.string.audio_media_player,
-                Toast.LENGTH_LONG).show();
+        if (player.getPlayWhenReady() == true &&
+                mPendingFile != null) {// if this is null we are already playing a file
+
+            mMediaPlayer = player;
+            //ready to play
+            mCurrentFile = mPendingFile;
+            mPendingFile = null;
+            Media media = getMedia(mCurrentFile);
+            if (media.duration == Media.DURATION_UNKNOWN) {
+                long duration = mMediaPlayer.getDuration();
+                media.duration = duration;
+            }
+        }
+        notifyState();
+        mHandler.sendEmptyMessageDelayed(1, 1000);
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlayer player) {
+
+        mMediaPlayer = player;
+
+        try {
+            cleanCurrentPLayer();
+        }
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        //Toast.makeText(mContext,R.string.audio_media_player,Toast.LENGTH_SHORT).show();
 
         Set<Map.Entry<AudioPlayerHolder, String>> entries = mAudioToPlayer.entrySet();
         for (Map.Entry<AudioPlayerHolder, String> entry : entries) {
@@ -349,6 +432,22 @@ public class AudioPlayer implements AudioPlayerHolder.AudioPlayerProvider, Media
             }
         }
 
-        return false;
     }
+
+    protected String getApplicationName() {
+
+        final PackageManager pm = mContext.getPackageManager();
+        ApplicationInfo ai;
+        try {
+            ai = pm.getApplicationInfo(mContext.getPackageName(), 0);
+            final String applicationName = (String) (ai != null ? pm.getApplicationLabel(ai) : "(unknown)");
+            return applicationName;
+        }
+        catch (final PackageManager.NameNotFoundException e) {
+            ai = null;
+        }
+
+        return "(unknown)";
+    }
+
 }
